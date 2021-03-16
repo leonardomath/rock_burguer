fa1fd050edca096943020c96b22daa1dc0c854e9
Victor Alves <sadbuttrue237@gmail.com>
2021-03-16 20:09:20 -0300

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'storage.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG'] = True
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return '<h1>index</h1>'
+
+@app.route('/reservar_mesa', methods=['POST'])
+def reservar_mesa():
+    pass
+
+@app.route('/trabalhe_conosco', methods=['POST'])
+def salvar_contato():
+    pass
+
+app.run()
