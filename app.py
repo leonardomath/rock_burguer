ebaa7479e1fe6149c673baf5fac028d4b510f273
Victor Alves <victor.rob237@gmail.com>
2021-03-16 22:02:08 -0300

    criado layout base, inicio da estrutura da pagina inical e arquivos estaticos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return 'hello'
+    return render_template('index.html')
 
 @app.route('/reservar_mesa', methods=['POST'])
 def reservar_mesa():
